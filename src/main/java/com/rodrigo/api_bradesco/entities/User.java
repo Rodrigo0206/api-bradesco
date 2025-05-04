4ce0fe1f2465ab91e866e5b5b14c9f717a0f7393
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2025-05-04 11:43:06 -0300

    Habilite ai anotaçÃão do Lombok nas entidade user

--- src/main/java/com/rodrigo/api_bradesco/entities/User.java
+++ src/main/java/com/rodrigo/api_bradesco/entities/User.java
@@ -1,55 +1,22 @@
 package com.rodrigo.api_bradesco.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_user")
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id ;
+    private Long id;
     private String name;
     private String email;
 
     @ManyToOne
     @JoinColumn(name = "department_id")
     private Department department;
-
-    public User(){
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
 }
