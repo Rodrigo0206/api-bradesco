dfeb57c9644e98b3a4890ffb482e28a47b2e1c1d
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2025-05-04 11:45:27 -0300

    habilitei o lombok no department

--- src/main/java/com/rodrigo/api_bradesco/entities/Department.java
+++ src/main/java/com/rodrigo/api_bradesco/entities/Department.java
@@ -1,33 +1,17 @@
 package com.rodrigo.api_bradesco.entities;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_department")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-    public Department() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
